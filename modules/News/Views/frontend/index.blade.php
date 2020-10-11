2dfebc6709d241e189b930cc41cdcfe6e5663f36
Kiran Maniya <vkiranmaniya@gmail.com>
2020-10-12 00:33:00 +0530

    #39 new module banner height

--- modules/News/Views/frontend/index.blade.php
+++ modules/News/Views/frontend/index.blade.php
@@ -5,34 +5,6 @@
     <link rel="stylesheet" type="text/css" href="{{ asset("libs/daterange/daterangepicker.css") }}">
     <link rel="stylesheet" type="text/css" href="{{ asset("libs/ion_rangeslider/css/ion.rangeSlider.min.css") }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset("libs/fotorama/fotorama.css") }}"/>
-
-    <style type="text/css">
-        .bravo-contact-block .section{
-            padding: 80px 0 !important;
-        }
-
-        .bravo_header.fixed-top{
-            background-color: white;
-        }
-
-        .bravo_wrap .bravo_header .content .header-left .bravo-menu ul li a{
-            transition: all .2s ease-out;
-            color: #1a2b48;
-            text-transform: none;
-            font-weight: bold;
-        }
-
-        .bravo_header .content img{
-            max-height: 65px;
-            filter: unset;
-        }
-
-        .bravo_header .container-fluid{
-            background-color: white;
-            box-shadow: 1px 0 1px #1a2b48;
-            /*box-shadow: 2px 3px 8px rgba(0,0,0,.1);*/
-        }
-    </style>
 @endsection
 @section('content')
     <div class="bravo-news">
