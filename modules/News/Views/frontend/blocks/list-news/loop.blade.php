8e3fcf0b220550d06e354aff80bf2b6a31420e3a
Kiran Maniya <vkiranmaniya@gmail.com>
2020-11-03 23:30:48 +0530

    #53 Post item text line limit | SEO Image field reposition

--- modules/News/Views/frontend/blocks/list-news/loop.blade.php
+++ modules/News/Views/frontend/blocks/list-news/loop.blade.php
@@ -28,7 +28,7 @@
                 <li class="dot"> {{ display_date($row->updated_at)}}  </li>
             </ul>
         </div>
-        <h3 class="item-title"><a href="{{$row->getDetailUrl()}}"> {{$translation->title}} </a></h3>
+        <h3 class="item-title"><a style="overflow: hidden;text-overflow: ellipsis;display: -webkit-box;-webkit-line-clamp: 2;-webkit-box-orient: vertical;" href="{{$row->getDetailUrl()}}"> {{$translation->title}} </a></h3>
         <div class="item-desc">
             {!! get_exceprt($translation->content,70,"...") !!}
         </div>
